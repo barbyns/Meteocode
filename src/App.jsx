a491513589cca42c51084d887f31734011d797ce
barbyns <barbyns95@gmail.com>
2025-04-11 10:37:03 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+import viteLogo from '/vite.svg'
+import './App.css'
+import SearchBar from './components/SearchBar'
+
+function App() {
+  return (
+    <>
+      <SearchBar/>
+    </>
+  )
+}
+
+export default App
